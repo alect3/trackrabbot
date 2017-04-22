f67a413df767958869f0b08fd45b90ea45ab0080
Garrit (u5210404) <u5210404@anu.edu.au>
2017-04-22 23:05:28 +1000

    updated pid.py

--- pid.py
+++ pid.py
@@ -1,6 +1,11 @@
 class PID():
     def __init__(self):
-        pass
-
+        self.Kp  = 0
+		self.Ki = 0
+		self.Kd =  0
+	def set_PID(self,Kp,Ki,Kd)
+		self.Kp = Kp;
+		self.Ki = Ki;
+		self.Kd = Kd;
     def get_required_pwm():
         return 180
